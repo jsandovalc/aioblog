d08b81f0e4f48217bb336cf1764c2816f19c28b1
Jonathan Sandoval <jsandoval@utp.edu.co>
2015-04-29 17:53:29 -0500

    Add template for index
    Must send the parameters.

--- blog/blog.py
+++ blog/blog.py
@@ -9,7 +9,7 @@ from aiohttp import web
 
 db_host, db_port = 'localhost', 27017
 
-@aiohttp_jinja2.templat('index.jinja2')
+@aiohttp_jinja2.template('index.jinja2')
 @asyncio.coroutine
 def root(request):
     mongo = yield from asyncio_mongo.Connection.create(db_host, db_port)
@@ -17,14 +17,14 @@ def root(request):
     post = blog.post
     body = ''
     posts = yield from post.find(limit=10)
-    print(posts)
     for post in posts:
         body += post['body']
-    return web.Response(body=bytes(body, 'utf-8'))
+    return {}
 
 app = web.Application()
-aiohttp_jinja2.setup(app,
-                     aiohttp_jinja2.jinja2.FileSystemLoader('./templates/'))
+aiohttp_jinja2.setup(
+    app,
+    loader=aiohttp_jinja2.jinja2.FileSystemLoader('./blog/templates'))
 app.router.add_route('GET', '/', root)
 
 
