9d8bb628bd6df5e6b906f872020e0e499da54d10
Jonathan Sandoval <jsandoval@utp.edu.co>
2015-05-04 15:30:03 -0500

    Add config file

---
+++ blog/config.py
@@ -0,0 +1,11 @@
+SERVE = '0.0.0.0'
+HOST = 'localhost'
+PORT = 8080
+
+# mongodb configuration
+DATABASE = 'blog'
+DBHOST = 'localhost'
+DBPORT = 27017
+
+# templates
+TEMPLATESPATH = './blog/templates'
